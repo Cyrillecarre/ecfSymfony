4d21c9c243940f320374a8d678f45168891b0873
Cyrille CARRE <cyrille.carre@gmail.com>
2024-03-15 16:43:41 +0100

    ajout admin

---
+++ src/Entity/Schedule.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ScheduleRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ScheduleRepository::class)]
+class Schedule
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $day = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE)]
+    private ?\DateTimeInterface $hours_o_matin = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE)]
+    private ?\DateTimeInterface $hours_f_matin = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE)]
+    private ?\DateTimeInterface $hours_o_a = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE)]
+    private ?\DateTimeInterface $hours_f_a = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDay(): ?string
+    {
+        return $this->day;
+    }
+
+    public function setDay(string $day): static
+    {
+        $this->day = $day;
+
+        return $this;
+    }
+
+    public function getHoursOMatin(): ?\DateTimeInterface
+    {
+        return $this->hours_o_matin;
+    }
+
+    public function setHoursOMatin(\DateTimeInterface $hours_o_matin): static
+    {
+        $this->hours_o_matin = $hours_o_matin;
+
+        return $this;
+    }
+
+    public function getHoursFMatin(): ?\DateTimeInterface
+    {
+        return $this->hours_f_matin;
+    }
+
+    public function setHoursFMatin(\DateTimeInterface $hours_f_matin): static
+    {
+        $this->hours_f_matin = $hours_f_matin;
+
+        return $this;
+    }
+
+    public function getHoursOA(): ?\DateTimeInterface
+    {
+        return $this->hours_o_a;
+    }
+
+    public function setHoursOA(\DateTimeInterface $hours_o_a): static
+    {
+        $this->hours_o_a = $hours_o_a;
+
+        return $this;
+    }
+
+    public function getHoursFA(): ?\DateTimeInterface
+    {
+        return $this->hours_f_a;
+    }
+
+    public function setHoursFA(\DateTimeInterface $hours_f_a): static
+    {
+        $this->hours_f_a = $hours_f_a;
+
+        return $this;
+    }
+}
