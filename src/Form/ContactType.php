8340610b8f976cf7bd3ae1a7066febf37f4c10e9
Cyrille CARRE <cyrille.carre@gmail.com>
2024-04-05 15:38:19 +0200

    Ajout securite sur endPoint.

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -8,6 +8,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Validator\Constraints as Assert;
 
 
 class ContactType extends AbstractType
@@ -16,8 +17,24 @@ class ContactType extends AbstractType
     {
         $builder
         ->add('email', EmailType::class)
-        ->add('subject', TextType::class)
-        ->add('message', TextareaType::class);
+        ->add('subject', TextType::class, [
+            'constraints' => [
+                new Assert\NotBlank(),
+                new Assert\Regex([
+                    'pattern' => '/^[a-zA-Z0-9\s]+$/',
+                    'message' => 'Le nom ne doit contenir que des lettres, des chiffres et des espaces.',
+                ]),
+            ],
+        ])
+        ->add('message', TextareaType::class, [
+            'constraints' => [
+                new Assert\NotBlank(),
+                new Assert\Regex([
+                    'pattern' => '/^[a-zA-Z0-9\s]+$/',
+                    'message' => 'Le contenu ne doit contenir que des lettres, des chiffres et des espaces.',
+                ]),
+            ],
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
--- src/Form/ReviewType.php
+++ src/Form/ReviewType.php
@@ -7,6 +7,9 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType; 
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ReviewType extends AbstractType
 {
@@ -16,12 +19,27 @@ class ReviewType extends AbstractType
             ->add('count', IntegerType::class, [
                 'mapped' => true,
             ])
-            ->add('name')
+            ->add('name', TextType::class, [
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Regex([
+                        'pattern' => '/^[a-zA-Z0-9\s]+$/',
+                        'message' => 'Le nom ne doit contenir que des lettres, des chiffres et des espaces.',
+                    ]),
+                ],
+            ])
             ->add('date', null, [
                 'widget' => 'single_text',
             ])
-            ->add('content');
-            
+            ->add('content', TextareaType::class, [
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Regex([
+                        'pattern' => '/^[a-zA-Z0-9\s]+$/',
+                        'message' => 'Le contenu ne doit contenir que des lettres, des chiffres et des espaces.',
+                    ]),
+                ],
+            ]);       
     }
 
     public function configureOptions(OptionsResolver $resolver): void
